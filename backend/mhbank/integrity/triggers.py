d9c42222458659ddba6fce9c7e5871b5678e90db
ahak14 <ah.abbasi.ah@gmail.com>
2021-02-04 10:27:24 +0330

    fixed a bug in triggers.py

--- backend/mhbank/integrity/triggers.py
+++ backend/mhbank/integrity/triggers.py
@@ -5,7 +5,7 @@ def phone_trigger():
     with connection.cursor() as cursor:
         cursor.execute(
             "Create trigger phone_trigger " +
-            "before insert of phone on Account " +
+            "before insert on Account " +
             "referencing new as nt " +
             "for each row when(" +
             "nt.phone like '09%' " +
@@ -17,9 +17,21 @@ def stt_comment_trigger():
     with connection.cursor() as cursor:
         cursor.execute(
             "Create trigger comment_trigger " +
-            "before insert of comment on STT " +
+            "before insert on STT " +
             "referencing new as nstt " +
             "for each row when(" +
             "LEN(nstt.comment) > 10000 " +
             "SIGNAL.SQL State '7005' ('comment is very long'))"
+        )
+
+
+def st_level_trigger():
+    with connection.cursor() as cursor:
+        cursor.execute(
+            "Create trigger st_level_trigger " +
+            "before insert on ST " +
+            "referencing new as nst " +
+            "for each row when(" +
+            "nst.level < 1 AND nst.level > 5 " +
+            "SIGNAL.SQL State '7005' ('level isn't in range'))"
         )
